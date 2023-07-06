6a0b5d69ff87c61c3165128f0e3c1b4cb359ca90
Patatalmeja <elmasprodante@hotmail.com>
2023-07-06 13:57:07 +0200

    reformando los parametros con nombre

--- 01-Dart-intro/05_functions.dart
+++ 01-Dart-intro/05_functions.dart
@@ -4,6 +4,7 @@ void main (){
 
    print( 'suma: ${ addTwoNumbers( 10,20)}');
    
+   print( greetPerson(  name: 'Fernando', message: 'Hi,'));
 }
 
 String greetEveryone() => 'Hello Everyone';
@@ -13,5 +14,9 @@ int addTwoNumbersOptional( int a, [ int b=0]) {
    //b ??=0
    
    return a + b ;
-
 }
+
+  String greetPerson({  required String name, String message = 'hola,'}){
+    return '$message Fernando';
+  
+  }
