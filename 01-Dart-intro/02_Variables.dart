45d3b3563756c9dc475d7cd28487c3e7331cfae3
Patatalmeja <elmasprodante@hotmail.com>
2023-07-06 06:11:19 +0200

    Practica de variables

---
+++ 01-Dart-intro/02_Variables.dart
@@ -0,0 +1,22 @@
+void main () {
+  
+  final String pokemon = 'ditto';
+  final int hp = 100;
+  final bool isAlive = true;
+  final List<String> abilidades = ['culeada extrema'];
+  final sprites = <String>['fotodeditto en tanga'];
+    
+    
+   print ("""
+   
+   $pokemon
+   $hp
+   $isAlive
+   $abilidades
+   $sprites
+   
+   
+   """);
+  
+  
+}
