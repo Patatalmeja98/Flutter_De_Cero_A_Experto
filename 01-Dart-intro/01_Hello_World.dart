0591959414f042ecafcc25802114dc4c3f372753
Patatalmeja <elmasprodante@hotmail.com>
2023-07-06 06:01:02 +0200

    Seccion 1 dart intro

---
+++ 01-Dart-intro/01_Hello_World.dart
@@ -0,0 +1,10 @@
+main() {
+  
+  //   var myName = 'NUñez';
+  String myName = 'Nuñez Alegria';
+  
+  
+   print ('Hola $myName');
+   print ('Hola ${myName.toUpperCase() }');
+   print ('Hola ${1+1 }');
+}
