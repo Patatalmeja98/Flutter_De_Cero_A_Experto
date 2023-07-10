6c43302e4e3317a27bb70bb22f180b6c4c066cef
Patatalmeja <elmasprodante@hotmail.com>
2023-07-10 17:20:00 +0200

    nueva addicion

---
+++ 01-Dart-intro/13_try.dart
@@ -0,0 +1,35 @@
+void main() async {
+
+
+  print('Inicio del programa');
+
+  try {
+
+
+    final value = await httpGet(' https://fernando-herrera.com/cursos');
+    print('exito: $value');
+
+  } on Exeption catch(err){
+    print('Tenemos una Exception: $err');
+
+  } catch (err) {
+    print('OOp¡¡ algo terrible paso: $err');
+
+  } finally {
+    print('Fin del try y catch');
+
+  }
+
+
+  print('Fin del programa');
+}
+
+Future<String> httpGet( String url) async {
+
+  await Future.delayed( const Duration(seconds: 1));
+
+  throw Exception('no hay parametros en el URL');
+
+  // throw 'erro de la peticion'
+  // return 'tenemos un valor de la peticion'
+}
