ca33b836f7a8e3bab56cb8507eb12d743165b66a
Patatalmeja <elmasprodante@hotmail.com>
2023-07-24 16:07:40 +0200

    Agregando estructua de directiorios

--- 02 -Prueba_flutter_application_1/lib/main.dart
+++ 02 -Prueba_flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Presentation/screens/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body: Center(
-        child: Text('hola putos'),
-      )),
-    );
+        debugShowCheckedModeBanner: false, home: CounterScreen());
   }
 }
