e3ed6ad4eccffd35ff1937370e5541dda1f769c1
Patatalmeja <elmasprodante@hotmail.com>
2023-08-04 18:07:24 +0200

    actualizacion de contador ya terminado

--- 02 -Prueba_flutter_application_1/lib/Presentation/screens/counter_screen.dart
+++ 02 -Prueba_flutter_application_1/lib/Presentation/screens/counter_screen.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
   const CounterScreen({super.key});
 
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('counter Screan'),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              '10',
-              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              '$clickCounter',
+              style:
+                  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
             ),
-            Text('clicks',
+            const Text('clicks',
                 style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100))
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
         child: const Icon(Icons.plus_one),
       ),
     );
