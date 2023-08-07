667a7a5e72bb259ac8e310ffaa8108d3622a305b
Patatalmeja <elmasprodante@hotmail.com>
2023-08-07 13:10:03 +0200

    List view and ChatSrean

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/chat/chat_scree.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,20 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'yes no app',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 6).theme(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: Center(
-          child: FilledButton.tonal(
-            onPressed: () {},
-            child: const Text('clickeame'),
-          ),
-        ),
-      ),
-    );
+        title: 'yes no app',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 1).theme(),
+        home: const ChatScreen());
   }
 }
---
+++ yes_no_app/lib/presentation/chat/chat_scree.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://i0.wp.com/imgs.hipertextual.com/wp-content/uploads/2020/07/hipertextual-conoce-fortuna-que-tenia-tony-stark-iron-man-universo-marvel-2020542291.jpg?fit=1920%2C1200&quality=50&strip=all&ssl=1'),
+          ),
+        ),
+        title: const Text(' mi mao da'),
+        centerTitle: false,
+      ),
+      body: _ChatVew(),
+    );
+  }
+}
+
+class _ChatVew extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice: $index');
+                },
+              ),
+            ),
+            Text('MUNDO')
+          ],
+        ),
+      ),
+    );
+  }
+}
