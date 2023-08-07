6a51387f0668c59eb04b7b7385419bb8c7c033f9
Patatalmeja <elmasprodante@hotmail.com>
2023-08-07 06:18:18 +0200

    estilo global para la aplicacion

---
+++ yes_no_app/lib/config/theme/app_theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const Color _customColor = Color(0xFF5C11D4);
+
+const List<Color> _colothemes = [
+  _customColor,
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      : assert(selectedColor <= 0 && selectedColor: 6,
+            'colors must be between 0 ${_colothemes.length}');
+
+  ThemeData theme() {
+    return ThemeData(
+        useMaterial3: true, colorSchemeSeed: _colothemes[selectedColor]);
+  }
+}
