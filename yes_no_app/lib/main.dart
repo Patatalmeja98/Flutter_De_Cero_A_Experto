23ba195cba85b8eb144ecc1b78a2a847a18a8938
Patatalmeja <elmasprodante@hotmail.com>
2023-08-09 10:52:47 +0200

    instalando providers

--- yes_no_app/lib/main.dart
+++ yes_no_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:yes_no_app/config/theme/app_theme.dart';
+import 'package:yes_no_app/presentation/providers/chat_providers.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_scree.dart';
 
 void main() => runApp(const MyApp());
@@ -9,10 +11,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'yes no app',
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme(selectedColor: 6).theme(),
-        home: const ChatScreen());
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (_) => ChatProvider())],
+      child: MaterialApp(
+          title: 'yes no app',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme(selectedColor: 6).theme(),
+          home: const ChatScreen()),
+    );
   }
 }
