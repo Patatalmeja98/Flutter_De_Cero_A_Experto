d1479460875f7ea65a819f16676f8c238a40cc87
Patatalmeja <elmasprodante@hotmail.com>
2023-08-15 21:33:34 +0200

    numeros nuevamente legibles

--- toktik/lib/presentation/screens/discover/discover_screen.dart
+++ toktik/lib/presentation/screens/discover/discover_screen.dart
@@ -1,15 +1,26 @@
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik/presentation/providers/discover_providers.dart';
+import 'package:toktik/presentation/widgets/shared/video_scrollable_viewe.dart';
 
 class DiscoverScrean extends StatelessWidget {
   const DiscoverScrean({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     body:  Center(
-     child: Text('discoveri screan'),
-     ),
+
+    final discoverProvider = context.watch<DiscoverProvidder>();
+
+
+    return   Scaffold(
+      body:  discoverProvider.initialLoading
+      ? const Center(child: CircularProgressIndicator(strokeWidth:  2))
+      : VideoScrollableView(videos: discoverProvider.videos)
+      
     );
+
+
+   
   }
 }
