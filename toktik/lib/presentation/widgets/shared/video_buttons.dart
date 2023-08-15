510faa55fb8e1144bd30f3a68e5219eaf2a41875
Patatalmeja <elmasprodante@hotmail.com>
2023-08-15 21:46:34 +0200

    bonus icono girando

--- toktik/lib/presentation/widgets/shared/video_buttons.dart
+++ toktik/lib/presentation/widgets/shared/video_buttons.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:toktik/config/helpers/human_formats.dart';
 import 'package:toktik/domain/video_post.dart';
@@ -19,7 +20,18 @@ class VideoButtons extends StatelessWidget {
     return Column(
       children: [
         _CustomIconButton( value: video.likes, iconColor: Color.fromARGB(255, 219, 17, 255), iconData: Icons.favorite, ),
-        _CustomIconButton( value: video.views, iconData:  Icons.remove_red_eye_outlined, ),
+         const SizedBox(height: 20,),
+        SpinPerfect(
+          infinite: true,
+          duration: const Duration(seconds: 5) ,
+          
+          child: _CustomIconButton( value: video.views, iconData:  Icons.remove_red_eye_outlined, )),
+         const SizedBox(height: 20,),
+       SpinPerfect(
+        infinite: true,
+        duration: const Duration(seconds: 5),
+          child: const _CustomIconButton( value:0, iconData:  Icons.play_circle_outline, )),
+        
         ],
     );
   }
@@ -45,7 +57,7 @@ class _CustomIconButton extends StatelessWidget {
         IconButton(
           onPressed: (){}, 
           icon: Icon(iconData, color:color, size: 30,)),
-        
+          if (value > 0)
         Text(HumanFormats.humanReadbleNumer(value.toDouble())),
       ],
     );
