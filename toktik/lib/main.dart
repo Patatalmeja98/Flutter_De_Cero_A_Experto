bf844da415f2ff4d6ca9d93368662bc0e073ea09
Patatalmeja98 <elmasprodante@hotmail.com>
2023-09-14 05:29:11 +1100

    comienzo de nuevo proyecto

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
-import 'package:toktik/domain/repositories/video_post_repositori.dart';
 import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
 import 'package:toktik/infrastructure/repositories/video_posts_repsitory_imp.dart';
 import 'package:toktik/presentation/providers/discover_providers.dart';
